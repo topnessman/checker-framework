be6b7996785cefc887085daf89ca16c09630309d
Jianchu <jianchuli0624@gmail.com>
2016-04-29 12:11:38 -0400

    move checkout in right place

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -10,10 +10,10 @@ if [ -d ../annotation-tools ] ; then
     # Older versions of git don't support the -C command-line option
     (cd ../annotation-tools && git pull)
 else
-    (cd .. && git clone https://github.com/pascaliUWat/annotation-tools.git && git checkout pascaliuwat)
+    (cd .. && git clone https://github.com/pascaliUWat/annotation-tools.git)
 fi
 # This also builds jsr308-langtools
-(cd ../annotation-tools/ && ./.travis-build-without-test.sh)
+(cd ../annotation-tools/ && git checkout pascaliuwat && ./.travis-build-without-test.sh)
 
 ## Compile
 ant dist
